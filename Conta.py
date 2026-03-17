00ce436bb7a5614ea918e6160859501838a4389b
prof-salu <saluorodrigues@gmail.com>
2026-03-17 15:33:19 -0300

    Encapsulamento com property

--- Conta.py
+++ Conta.py
@@ -1,25 +1,56 @@
 class Conta:
+    # _  ==> protegido
+    # __ ==> privado
     def __init__(self, numero, titular, saldo):
-        #Encapsulando os atributos
-        #_  protegido (protected) (apenas as classes filhas tem acesso)
-        #__ privado (private) (apenas a propria classe tem acesso)
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
 
-    #GET ==> captura a variavel (sempre com retorno)
-    def get_saldo(self):
+    #Equivalente ao GET
+    @property #decorator
+    def titular(self):
+        return self.__titular
+
+    #Equivalente ao SET
+    @titular.setter #decorator
+    def titular(self, titular):
+        if len(titular) > 1:
+            self.__titular = titular
+        else:
+            print('O nome deve ter mais de 1 caractere')
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def saldo(self):
         return self.__saldo
 
-    #SET ==> altera uma variavel (sempre sem retorno)
+    #O usuario altera o saldo pelas funcoes de saque e deposito
     def saque(self, valor):
-        if self.__saldo - valor <= 0 or valor < 0:
-            print('Saldo insuficiente ou valor invalido para o saque')
-        else:
+        if self.__saldo - valor > 0 and valor > 0:
             self.__saldo -= valor
+            return True
+        else:
+            print('Saldo insuficiente ou valor invalido')
+            return False
 
     def deposito(self, valor):
-        if valor <= 0:
-            print('Valor inválido para deposito')
-        else:
+        if valor > 0:
             self.__saldo += valor
+        else:
+            print('Valor invalido')
+
+    def transfere(self, valor, favorecido):
+        if self.saque(valor):
+            favorecido.deposito(valor)
+
+
+    '''
+    def get_titular(self):
+        return self.__titular
+
+    def set_titular(self, titular):
+        self.__titular = titular
+    '''
--- Main.py
+++ Main.py
@@ -1,15 +1,40 @@
 from Conta import Conta
 
-cc = Conta('1234', 'Juca', 1000)
-print(f'Saldo: {cc.get_saldo()}')
-#saque de 500
+cc1 = Conta(1234, 'Juca', 1000)
 
-cc.saque(500)
-cc.saque(700)
+#print(f'Titular com get: {cc1.get_titular()}')
+print(f'Titular com property: {cc1.titular}')
 
-print(f'Saldo: {cc.get_saldo()}')
+#alterando o titular via funcao set
+#cc1.set_titular('Juca Pereira')
 
-cc.deposito(-300)
+#alterando o titular via property
+cc1.titular = 'Juca Pereira'
 
-#Usuario nao pode ter saldo negativo
-print(f'Saldo: {cc.get_saldo()}')
+#print(f'Titular alterado com set: {cc1.get_titular()}')
+print(f'Titular alterado com property: {cc1.titular}')
+
+cc1.titular = 'Li'
+
+print(f'Titular alterado com property: {cc1.titular}')
+
+#deposito de 500 reais
+cc1.deposito(500)
+
+#saque de 150 reais
+cc1.saque(150)
+
+print(f'Saldo atual: R${cc1.saldo:.2f}')
+
+#cc1.numero = 5
+print(f'Numero: {cc1.numero}')
+
+cc2 = Conta(1111, 'Ana', 1000)
+cc3 = Conta(2222, 'Mauro', 700)
+
+print(f'Saldo antes da transferencia [cc2] {cc2.saldo}:')
+print(f'Saldo antes da transferencia [cc3] {cc3.saldo}:')
+#transferir 200 de cc2 para cc3
+cc2.transfere(2000, cc3)
+print(f'Saldo depois da transferencia [cc2] {cc2.saldo}:')
+print(f'Saldo depois da transferencia [cc3] {cc3.saldo}:')
